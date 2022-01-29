994f43cb448630aab7083d44712ddd095fd2f891
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2022-01-29 16:48:16 +0530

    Initial commit of the project.
    This commit adds the necessary boilerplate files to start an LSP client
    for prisma schema language.

---
+++ plugin.py
@@ -0,0 +1,20 @@
+import os
+from lsp_utils import NpmClientHandler
+
+
+def plugin_loaded():
+    LspPrismaPlugin.setup()
+
+
+def plugin_unloaded():
+    LspPrismaPlugin.cleanup()
+
+
+class LspPrismaPlugin(NpmClientHandler):
+    package_name = __package__
+    server_directory = "language-server"
+    server_binary_path = os.path.join(
+        server_directory,
+        "out",
+        "server.js",
+    )
