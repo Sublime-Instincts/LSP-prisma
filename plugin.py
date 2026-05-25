b7749321c656c1afd44bb95ff726811bb921f284
Rafał Chłodnicki <rchl2k@gmail.com>
2026-05-25 20:27:54 +0200

    refactor: migrate to LspPlugin (#4)

--- plugin.py
+++ plugin.py
@@ -1,20 +1,31 @@
-import os
-from lsp_utils import NpmClientHandler
+from __future__ import annotations
+
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from lsp_utils import NodeManager
+from pathlib import Path
+from sublime_lib import ResourcePath
+from typing_extensions import override
 
 
 def plugin_loaded():
-    LspPrismaPlugin.setup()
+    LspPrismaPlugin.register()
 
 
 def plugin_unloaded():
-    LspPrismaPlugin.cleanup()
+    LspPrismaPlugin.unregister()
 
 
-class LspPrismaPlugin(NpmClientHandler):
-    package_name = __package__
-    server_directory = "language-server"
-    server_binary_path = os.path.join(
-        server_directory,
-        "node_modules",
-        "@prisma", "language-server", "dist", "bin.js"
-    )
+class LspPrismaPlugin(LspPlugin):
+
+    @classmethod
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath("Packages", package_name, "language-server"),
+            Path("node_modules", "@prisma", "language-server", "dist", "bin.js"),
+            node_version_requirement=">=20",
+        )
